a867daa1b63497274473e3194ab5cd6f78540285
matheusepifanio1 <45240387+matheusepifanio1@users.noreply.github.com>
2022-03-15 20:19:49 -0300

    first

---
+++ src/components/modals/InfoModal.tsx
@@ -0,0 +1,69 @@
+import { Cell } from '../grid/Cell'
+import { BaseModal } from './BaseModal'
+
+type Props = {
+  isOpen: boolean
+  handleClose: () => void
+}
+
+export const InfoModal = ({ isOpen, handleClose }: Props) => {
+  return (
+    <BaseModal title="How to play" isOpen={isOpen} handleClose={handleClose}>
+      <p className="text-sm text-gray-500 dark:text-gray-300">
+        Guess the word in 6 tries. After each guess, the color of the tiles will
+        change to show how close your guess was to the word. All the words were taken from GodsGame environment.
+      </p>
+
+      <div className="flex justify-center mb-1 mt-4">
+        <Cell
+          isRevealing={true}
+          isCompleted={true}
+          value="G"
+          status="correct"
+        />
+        <Cell value="O" />
+        <Cell value="D" />
+        <Cell value="S" />
+      </div>
+      <p className="text-sm text-gray-500 dark:text-gray-300">
+        The letter G is in the word and in the correct spot.
+      </p>
+
+      <div className="flex justify-center mb-1 mt-4">
+        <Cell value="H" />
+        <Cell value="E" />
+        <Cell
+          isRevealing={true}
+          isCompleted={true}
+          value="A"
+          status="present"
+        />
+        <Cell value="D" />
+      </div>
+      <p className="text-sm text-gray-500 dark:text-gray-300">
+        The letter A is in the word but in the wrong spot.
+      </p>
+
+      <div className="flex justify-center mb-1 mt-4">
+        <Cell value="T" />
+        <Cell value="H" />
+        <Cell value="O" />
+        <Cell isRevealing={true} isCompleted={true} value="R" status="absent" />
+      </div>
+      <p className="text-sm text-gray-500 dark:text-gray-300">
+        The letter R is not in the word in any spot.
+      </p>
+
+      <p className="mt-6 italic text-sm text-gray-500 dark:text-gray-300">
+        This is an open source version of the word guessing game we all know and
+        love -{' '}
+        <a
+          href="https://github.com/cwackerfuss/react-wordle"
+          className="underline font-bold"
+        >
+          check out the code here
+        </a>{' '}
+      </p>
+    </BaseModal>
+  )
+}
